31c291c4a9abf58f7455c1980a71bb7372897677
Pratik-03-12 <sahapratik390@gmail.com>
2024-06-17 12:22:34 +0530

    added top loading bar and fixed major issues

--- src/components/News.js
+++ src/components/News.js
@@ -30,16 +30,20 @@ export class News extends Component {
   }
   
   async updateNews(){
+    this.props.setProgress(10);
     const url =
       `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=559c6cf3c79e44c99769c705b21342fa&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({loading:true});
     let data = await fetch(url);
+    this.props.setProgress(40);
     let parsedData = await data.json();
+    this.props.setProgress(70);
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
       loading:false
     });
+    this.props.setProgress(100);
   }
   async componentDidMount() {
     this.updateNews();
